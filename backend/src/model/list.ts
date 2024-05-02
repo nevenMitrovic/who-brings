3e6fe8f0293c76688b2ebb60228b6e37c8c36cf4
nevenMitrovic <nevenmitrovic4@gmail.com>
2024-05-02 09:58:27 +0200

    list mongoose model created

---
+++ backend/src/model/list.ts
@@ -0,0 +1,19 @@
+import { Schema, InferSchemaType, model } from "mongoose";
+
+const listSchema = new Schema({
+    name: { String },
+    date: { String },
+    location: { String },
+    description: { String },
+    items: [{
+        name: { String },
+        description: { String },
+        amount: { Number },
+        unit: { String },
+    }],
+},
+    { timestamps: true });
+
+type List = InferSchemaType<typeof listSchema>;
+
+export default model<List>("list", listSchema);
