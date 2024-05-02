9838821d636087d8e3300fdca3408d65ba1964dc
nevenMitrovic <nevenmitrovic4@gmail.com>
2024-05-02 21:18:44 +0200

    updateList and createItem added in controller and routes

--- backend/src/controller/list.ts
+++ backend/src/controller/list.ts
@@ -3,7 +3,9 @@ import createHttpError from "http-errors";
 import ListModel from "../model/list";
 import mongoose from "mongoose";
 
-export const getList: RequestHandler = async (req, res, next) => {
+
+
+export const getList: RequestHandler<ListParams, unknown, List, unknown> = async (req, res, next) => {
     const listId = req.params.listId;
     try {
         if (!mongoose.isValidObjectId(listId)) {
@@ -12,7 +14,7 @@ export const getList: RequestHandler = async (req, res, next) => {
 
         const list = await ListModel.findById(listId).exec();
 
-        if(!list) {
+        if (!list) {
             throw createHttpError(404, "List not found");
         }
 
@@ -39,10 +41,7 @@ interface List {
 
 export const createList: RequestHandler<unknown, unknown, List, unknown> = async (req, res, next) => {
     const listName = req.body.name;
-    const listDate = req.body.date;
-    const listLocation = req.body.location;
-    const listDescription = req.body.description;
-    const listItems = req.body.items;
+
     try {
         if (!listName) {
             throw createHttpError(400, "List name missing");
@@ -50,13 +49,86 @@ export const createList: RequestHandler<unknown, unknown, List, unknown> = async
 
         const newList = await ListModel.create({
             name: listName,
-            date: listDate,
-            location: listLocation,
-            description: listDescription,
-            items: listItems
         });
 
         res.status(201).json(newList);
+    } catch (error) {
+        next(error);
+    }
+};
+
+interface ListParams {
+    listId: String
+    itemId: String
+};
+
+export const updateList: RequestHandler<ListParams, unknown, List, unknown> = async (req, res, next) => {
+    const listId = req.params.listId;
+    const newListName = req.body.name;
+    const newListDate = req.body.date;
+    const newListLocation = req.body.location;
+    const newListDescription = req.body.description;
+
+    try {
+        if (!mongoose.isValidObjectId(listId)) {
+            throw createHttpError(400, "Invalid list ID");
+        }
+
+        const list = await ListModel.findById(listId).exec();
+
+        if (!list) {
+            throw createHttpError(404, "List not found");
+        }
+
+        if (newListName) {
+            list.name = newListName;
+        }
+
+        if (newListDate) {
+            list.date = newListDate;
+        }
+
+        if (newListLocation) {
+            list.location = newListLocation;
+        }
+
+        if (newListDescription) {
+            list.description = newListDescription;
+        }
+
+        const updatedList = await list.save();
+
+        res.status(200).json(updatedList);
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const createItem: RequestHandler<ListParams, unknown, Item, unknown> = async (req, res, next) => {
+    const listId = req.params.listId;
+    const { name, description, amount, unit } = req.body;
+
+    try {
+        if (!name) {
+            throw createHttpError(404, "Item name missing");
+        }
+
+        if (!mongoose.isValidObjectId(listId)) {
+            throw createHttpError(400, "Invalid list ID");
+        }
+
+        const list = await ListModel.findById(listId).exec();
+
+        if (!list) {
+            throw createHttpError(404, "List not found");
+        }
+
+        list.items.push({ name, description, amount, unit });
+
+        const createdItem = await list.save();
+
+        res.status(200).json(createdItem);
     } catch (error) {
         next(error);
     }
--- backend/src/routes/list.ts
+++ backend/src/routes/list.ts
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.get('/:listId', controller.getList);
 router.post('/', controller.createList);
+router.put('/:listId', controller.updateList);
+router.put('/:listId/create', controller.createItem);
 
 export default router;
