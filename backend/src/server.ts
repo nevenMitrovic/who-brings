826cec5f48781360c19a0ca9ba0c9501489ef396
nevenMitrovic <nevenmitrovic4@gmail.com>
2024-05-02 09:19:30 +0200

    util envalidEnv created, server connected with mongoose, error handler created

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,13 @@
-import express from "express";
+import app from './app';
+import mongoose from 'mongoose';
+import env from "./util/validateEnv";
+
+const port = env.PORT;
+
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+.then(() => {
+    console.log("mongoose is connected");
+    app.listen(port, () => {
+        console.log("Server is connected on port: "+ port);
+    });
+});
