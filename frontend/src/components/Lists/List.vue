2488b3783468f33f5732de8e073117e65c181506
nevenMitrovic <nevenmitrovic4@gmail.com>
2024-05-10 16:08:38 +0200

    started with List component UI

--- frontend/src/components/Lists/List.vue
+++ frontend/src/components/Lists/List.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>{{ list.name }}</div>
+  <div class="flex justify-center">
+    <div class="w-1/2">
+      <div class="w-full max-width-full">
+        <div class="border-b border-gray-200 py-2">
+          <span class="text-lg text-gray-500">Who brings list for</span>
+          <div class="max-width-full truncate text-gray-500 font-bold text-xl">
+            {{ list.name }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -13,6 +24,9 @@ const id = route.params.id as string;
 let list = ref({});
 
 onMounted(() => {
-  listsService.getList(id).then((res) => {list.value = res; console.log(res)});
+  listsService.getList(id).then((res) => {
+    list.value = res;
+    console.log(res);
+  });
 });
 </script>
