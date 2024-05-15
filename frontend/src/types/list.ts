e7b2cbbc351fe1c6d2612fc0d22f6f9ed9d0011a
nevenMitrovic <nevenmitrovic4@gmail.com>
2024-05-15 17:21:40 +0200

    fix: Item type

--- frontend/src/types/list.ts
+++ frontend/src/types/list.ts
@@ -10,11 +10,5 @@ interface Item {
     name: string,
     description?: string,
     amount?: number,
-    unit?: Unit
+    unit?: string
 };
-
-enum Unit {
-    kilograms = 1,
-    centimeter = 2,
-    litres = 3,
-}
